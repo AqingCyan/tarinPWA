75e48c27c41f0e51eb5485db9c7a180f2a224d5c
AqingCyan <aqingcyan@cyanthing.com>
2020-08-19 02:28:14 +0800

    init: 初始化项目目录

---
+++ src/order/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+import './App.css'
+
+const App = (props) => {
+
+}
+
+export default connect(
+  function mapStateToProps(state) {},
+  function mapDispatchToProps(dispatch) {},
+)(App)
