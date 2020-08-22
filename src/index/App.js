4bb4ccb70c522de32ed61ce2f08953ee48c593dc
AqingCyan <aqingcyan@cyanthing.com>
2020-08-23 01:01:44 +0800

    ADD: 选择站点部分

--- src/index/App.js
+++ src/index/App.js
@@ -1,4 +1,5 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useMemo } from 'react'
+import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
 import Header from '../common/Header'
@@ -6,10 +7,11 @@ import Journey from './Journey'
 import DepartDate from './DepartDate'
 import HighSpeed from './HighSpeed'
 import Submit from './Submit'
-
+import { exchangeFromTo, showCitySelector } from './actions'
 import './App.css'
 
 const App = (props) => {
+  const { from, to, dispatch } = props
 
   /**
    * 头部栏点击返回的回调方法
@@ -18,12 +20,27 @@ const App = (props) => {
     window.history.back()
   }, [])
 
+  const callbacks = useMemo(() => {
+    return bindActionCreators({
+      // 切换始发终点站
+      exchangeFromTo,
+      // 点击打开选择站点的浮层
+      showCitySelector,
+    }, dispatch)
+  }, [])
+
   return (
     <div>
       <div className="header-wrapper">
         <Header title="火车票" onBack={onBack} />
       </div>
-      <Journey />
+      <form className="form">
+        <Journey
+          from={from}
+          to={to}
+          {...callbacks}
+        />
+      </form>
       <DepartDate />
       <HighSpeed />
       <Submit />
@@ -33,9 +50,9 @@ const App = (props) => {
 
 export default connect(
   function mapStateToProps(state) {
-    return {}
+    return state
   },
   function mapDispatchToProps(dispatch) {
-    return {}
+    return { dispatch }
   },
 )(App)
--- src/index/Journey.jsx
+++ src/index/Journey.jsx
@@ -1,12 +1,54 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
 import './Journey.css'
+import switchImg from './img/switch.svg'
 
 const Journey = props => {
+  const { from, to, exchangeFromTo, showCitySelector, } = props
+
   return (
-    <div>
-      <h1>Hello Header</h1>
+    <div className="journey">
+      <div
+        className="journey-station"
+        onClick={() => { showCitySelector(true) }}
+      >
+        <input
+          className="journey-input journey-from"
+          type="text"
+          readOnly
+          name="from"
+          value={from}
+        />
+      </div>
+      <div
+        className="journey-switch"
+        onClick={exchangeFromTo}
+      >
+        <img src={switchImg} width="70" height="40" alt="switch" />
+      </div>
+      <div
+        className="journey-station"
+        onClick={() => { showCitySelector(false) }}
+      >
+        <input
+          className="journey-input journey-to"
+          type="text"
+          readOnly
+          name="to"
+          value={to}
+        />
+      </div>
     </div>
   )
 }
+
+Journey.propTypes = {
+  from: PropTypes.string.isRequired,
+  to: PropTypes.string.isRequired,
+  exchangeFromTo: PropTypes.func.isRequired,
+  showCitySelector: PropTypes.func.isRequired,
+}
+
 
 export default Journey
